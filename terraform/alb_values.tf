b0829a90cab954be49ea75351e4c5697c58cd55f
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-07-13 19:27:15 +0530

    alv calue fix

---
+++ terraform/alb_values.tf
@@ -0,0 +1,37 @@
+resource "local_file" "aws_alb_values" {
+  filename = "${path.module}/../argoCD/environments/dev/aws_alb_values.yaml"
+
+  content = yamlencode({
+    fullnameOverride = "aws-load-balancer-controller"
+    clusterName      = aws_eks_cluster.eks_cluster.name
+    vpcId            = aws_vpc.main.id
+
+    replicaCount = 2
+
+    serviceAccount = {
+      name        = "aws-load-balancer-controller"
+      annotations = {}
+    }
+
+    ingressClassConfig = {
+      default = false
+    }
+
+    logLevel = "info"
+
+    resources = {
+      requests = {
+        cpu    = "100m"
+        memory = "128Mi"
+      }
+      limits = {
+        cpu    = "500m"
+        memory = "512Mi"
+      }
+    }
+
+    podDisruptionBudget = {
+      maxUnavailable = 1
+    }
+  })
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 }
 
